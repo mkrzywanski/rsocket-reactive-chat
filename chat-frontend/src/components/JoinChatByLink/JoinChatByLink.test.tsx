76d2cdef7590f72d9b58fe0493de21e613e1edde
mkrzywanski <michal.krzywanski1@gmail.com>
2022-06-16 11:56:17 +0200

    final

--- chat-frontend/src/components/JoinChatByLink/JoinChatByLink.test.tsx
+++ chat-frontend/src/components/JoinChatByLink/JoinChatByLink.test.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import JoinChatByLink from './JoinChatByLink';
-import { MemoryRouter, Router } from 'react-router-dom';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import JoinChatByLink from "./JoinChatByLink";
+import { MemoryRouter, Router } from "react-router-dom";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import Keycloak from "keycloak-js";
 
-describe('<JoinChatByLink />', () => {
-  test('it should mount', () => {
-    render(<MemoryRouter><JoinChatByLink addChat={() => {}}/></MemoryRouter>);
-    
-    const joinChatByLink = screen.getByTestId('JoinChatByLink');
+describe("<JoinChatByLink />", () => {
+  test("it should mount", () => {
+
+    const createKeycloakStub = () => ({
+      init: jest.fn().mockResolvedValue(true),
+      updateToken: jest.fn(),
+      login: jest.fn(),
+      logout: jest.fn(),
+      register: jest.fn(),
+      accountManagement: jest.fn(),
+      createLoginUrl: jest.fn()
+      
+    });
+    let mockInitialized = false;
+
+    jest.mock("Keycloak")
+    // jest.mock("@react-keycloak/web", () => {
+    //   const originalModule = jest.requireActual("@react-keycloak/web");
+    //   return {
+    //     ...originalModule,
+    //     useKeycloak: () => [createKeycloakStub(), mockInitialized],
+    //   };
+    // });
+
+    // render(
+    //   <ReactKeycloakProvider authClient={createKeycloakStub()}>
+    //     <MemoryRouter>
+    //       <JoinChatByLink addChat={() => {}} />
+    //     </MemoryRouter>
+    //   </ReactKeycloakProvider>
+    // );
+
+    const joinChatByLink = screen.getByTestId("JoinChatByLink");
 
     expect(joinChatByLink).toBeInTheDocument();
   });
